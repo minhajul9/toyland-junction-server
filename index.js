85803774f18bec7ef407f65583e7d4f62824f993
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-05-20 11:51:29 +0600

    search by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -46,11 +46,21 @@ async function run() {
     // search item
     app.get('/search', async(req, res) => {
         const search = req.query.search;
-        console.log(search);
+        // console.log(search);
         const query = { name: { $regex: search, $options: 'i'}}
         const result = await toysCollection.find(query).toArray();
         res.send(result)
     })
+
+    // find by id 
+    app.get('/toy/:id', async(req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        const query = { _id : new ObjectId(id)};
+        const result = await toysCollection.findOne(query);
+        res.send(result)
+    })
+
 
     // find by category
     app.post('/category', async(req, res) => {
